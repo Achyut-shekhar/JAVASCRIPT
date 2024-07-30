647a8e13fc7c9f6326057bb90565be81f80131a2
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-07-30 23:45:42 +0530

    project completed

--- project/java.js
+++ project/java.js
@@ -2,10 +2,77 @@ let userScore=0;
 let compScore=0;
 
 const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const user=document.querySelector("#user");
+const comp=document.querySelector("#comp");
+
+const drawgame=()=>{
+    console.log("game is draw!!!")
+     msg.innerText="Game draw.play again!!!"
+     msg.style.backgroundColor="black";
+}
+
+const gencompChoice=()=>{
+    const options= ["rock","paper","scissor"];
+    const RandIdx=Math.floor(Math.random()*3);
+    return options[RandIdx];
+}
+
+const showWinner=(userwin)=>{
+    if(userwin){
+        userScore++;
+        user.innerText=userScore;
+        console.log("you win!!");
+        msg.innerText = `You win!!`;
+
+        msg.style.backgroundColor="green";
+       
+    }
+    else
+    {compScore++;
+        comp.innerText=compScore;
+        console.log("you lost");
+ msg.innerText="You lost !!"
+  msg.style.backgroundColor="red";
+
+}
+};
+
+const playgame=(userchoice)=>{
+    console.log("user choice= ",userchoice)
+    //generate computer choice -> modular programming
+    let compChoice= gencompChoice();
+    console.log("comp choice= ",compChoice);
+
+    if(userchoice===compChoice){
+        //draw game
+        drawgame();
+    }
+    else{
+        let userwin=true;
+        if(userchoice==="rock"){
+            //scissor,paper
+           userwin= compChoice==="paper"?false:true;
+        }
+        else if(userchoice==="paper")
+        {
+            //rock,scissor
+            userwin=compChoice==="scissor" ? false:true;
+        } 
+        else if(userchoice==="scissor")
+        {
+           userwin=compChoice==="rock"?false:true;
+       }
+       showWinner(userwin);
+
+    }
+   
+   
+}
 
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
-        const choiceclass=choice.getAttribute("id")
-        console.log("choice was clicked",choiceclass);
+        const userchoice=choice.getAttribute("id")
+        playgame(userchoice);
     })
 })
